11ec3975fb811deb9d4d4e5334793c942c8ee014
Manuel Espinoza <manuel.partsplussv.com>
2019-10-31 11:39:32 -0600

    add env var for database

--- src/keys.js
+++ src/keys.js
@@ -1,9 +1,14 @@
 module.exports = {
 
     database: {
-        host: 'localhost',
-        user: 'root',
-        password: 'root',
-        database: 'socialmedia'
+        // host: 'localhost',
+        // user: 'root',
+        // password: 'root',
+        // database: 'socialmedia'
+
+        host     : process.env.MYSQL_ADDON_HOST || 'localhost',
+    	database : process.env.MYSQL_ADDON_DB || 'socialmedia',
+    	user     : process.env.MYSQL_ADDON_USER || 'root' ,
+    	password : process.env.MYSQL_ADDON_PASSWORD || 'root'
     }
 };
