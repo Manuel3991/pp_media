00db418b74b4db0d2a545b6566b887fc71d3d266
Manuel Espinoza <manuel.partsplussv.com>
2019-11-04 11:33:05 -0600

    fixed querys for users and add cancel button in forms for users

--- src/routes/users.js
+++ src/routes/users.js
@@ -13,12 +13,12 @@ router.get('/add',isLoggedIn, (req, res) => {
 });
 
 router.post('/add',isLoggedIn, async (req, res) => {
-  const { fullname, username, password, idrole } = req.body;
+  const { fullname, username, password, idtype_user } = req.body;
   const newUser = {
     fullname, 
     username, 
     password, 
-    idrole
+    idtype_user
   };
    console.log(newUser);
   await pool.query('INSERT INTO user set ?',[newUser]);
@@ -29,7 +29,7 @@ router.post('/add',isLoggedIn, async (req, res) => {
 
 router.get('/edit/:iduser',isLoggedIn, async (req,res)=>{
   const { iduser } = req.params;
-  const users = await pool.query('SELECT iduser,fullname,username,password,idrole FROM user WHERE iduser = ?',[iduser]);
+  const users = await pool.query('SELECT iduser,fullname,username,password,idtype_user FROM user WHERE iduser = ?',[iduser]);
   res.render('users/edit',{users: users[0]});
  //console.log(users);
 // res.send('Received');
@@ -54,7 +54,7 @@ router.get('/delete/:iduser',isLoggedIn, async(req,res)=>{
          
        const { iduser } = req.params; 
        
-       await pool.query('DELETE FROM user WHERE iduser = ? AND idrole NOT IN(1,2)',[iduser]);
+       await pool.query('DELETE FROM user WHERE iduser = ? AND idtype_user NOT IN(1,2)',[iduser]);
        res.redirect('/users');             
 });
 
@@ -67,7 +67,7 @@ router.get('/',isLoggedIn, async (req, res) => {
 
   // const users = await pool.query('SELECT user.iduser,user.fullname,user.username,user.password,role.role FROM user INNER JOIN role ON user.idrole=role.idrole');
  
-  const users = await pool.query('SELECT user.iduser AS iduser,user.fullname AS fullname,user.username AS username,role.role AS rolename FROM user INNER JOIN role using(idrole)');
+  const users = await pool.query('SELECT user.iduser, user.fullname, user.username, type_user.nametypeuser FROM user INNER JOIN type_user using(idtype_user)');
   console.log(users);
   res.render('users/list',{users});
  // res.render('users/list');
